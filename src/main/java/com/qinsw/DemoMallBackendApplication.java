4f63465cbaddc1a8467de82331c15a6608552637
qinsw <2573992956@qq.com>
2020-03-02 10:27:47 +0800

    init

---
+++ src/main/java/com/qinsw/DemoMallBackendApplication.java
@@ -0,0 +1,16 @@
+package com.qinsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sebastian
+ */
+@SpringBootApplication
+public class DemoMallBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMallBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qinsw/DemoMallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qinsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMallBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
